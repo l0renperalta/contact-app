fb18fcee217311231dde56b87eb6bc5b5340eb3e
loren <stinpc@hotmail.com>
2023-01-12 18:28:34 -0500

    startup config

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 5000);
+app.engine(
+   'hbs',
+   engine({
+      defaultLayout: 'main',
+      layoutsDir: path.join(app.get('views'), 'layouts'),
+      partialsDir: path.join(app.get('views'), 'partials'),
+      helpers: require('./lib/handlebars'),
+      extname: '.hbs',
+   })
+);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Global variables
+// app.use((req, res, next) => {
+//    next();
+// });
+
+// Routes
+app.use(require('./routes/index'));
+app.use(require('./routes/auth'));
+app.use('/links', require('./routes/links'));
+
+// Public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Starting server
+app.listen(app.get('port'), () => console.log(`Server listen on port ${app.get('port')}`));
